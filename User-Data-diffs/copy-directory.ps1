7060e04e6248bc79f57bfd1bdc7daae7a014c919
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-09-04 15:25:09 +0200

    n/a

--- User-Data-diffs/copy-directory.ps1
+++ User-Data-diffs/copy-directory.ps1
@@ -1,1 +1,15 @@
-copy-item -recurse "$env:LOCALAPPDATA\Google\Chrome\User Data"   DESTDIR
+$DESTDIR = ...
+
+copy-item   -recurse "$env:LOCALAPPDATA\Google\Chrome\User Data"   $DESTDIR
+
+remove-item -recurse "$DESTDIR/Default/Cache"
+remove-item -recurse "$DESTDIR/PepperFlash"
+remove-item -recurse "$DESTDIR/PepperFlash"
+remove-item -recurse "$DESTDIR/Safe Browsing"
+remove-item -recurse "$DESTDIR/Safe Browsing Cookies"
+remove-item -recurse "$DESTDIR/Safe Browsing Cookies-journal"
+
+remove-item -recurse "$DESTDIR/GrShaderCache"
+remove-item -recurse "$DESTDIR/Default/data_reduction_proxy_leveldb"
+remove-item -recurse "$DESTDIR/Default/Code Cache"
+remove-item -recurse "$DESTDIR/Default/Cache"
